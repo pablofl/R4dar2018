eedc09f538b4567732f9fa1e0443ea61f3f86cc5
Pablofl <pabloflopes@gmail.com>
2018-08-02 09:23:30 -0300

    Radar 1R4dar 2018
    Radar 1R4dar 2018

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# R4dar2018
+### Zap
+
+Antes do primeiro build LEIA ESSAS INSTRUÇÕES:
+
+a) Abra o arquivo org.telegram.messenger.BuildVars.java e preencha as credenciais que devem ser geradas no liks que estão nos comentários do mesmo
+
+b) no https://firebase.google.com/ (um dos links do passo anterior) é gerado o arquivo google-services.json, ele deve ser salvo dentro da pasta /TMessagesProj do projeto
+
+c) Altero as ultimas linhas do values/strings.xml de acordo com o seu app (nome do app mensagem de boas vindas e etc...)
+
+d) Substitua os arquivos ic_launcher.png nas pastas  /drawble pelo arquivo de seu app
+
+e) Altere o build.grade de acordo com o reverse name do seu app (IMPORTANTE: é o mesmo reverse name que está no arquivo google-services.json
+
+f) Gere sua chave de release e salve-a na pasta _signAPK como "release.keystore" e altere as credenciais de acordo no build.grande em "signingConfigs"
---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,25 @@
+package org.telegram.messenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static int BUILD_VERSION = 1155;
+    public static String BUILD_VERSION_STRING = "1.0";
+    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = ""; //https://rink.hockeyapp.net/
+    public static String HOCKEY_APP_HASH_DEBUG = "";
+    public static String HOCKEY_APP_HASH_DEBUG_SECRET = "";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String GOOGLE_API_KEY = "";
+    public static String GCM_SENDER_ID = "";
+    public static String FOURSQUARE_API_VERSION = "";
+
+    public static int NINJA_APP_MAIN_COLOR = 0xff672f79;
+
+    public static int NINJA_STATUS_ONLINE_COLOR = 0xffff2f79;
+    public static int NINJA_HOUR_COLOR = 0xff5f6060;
+    public static String SEND_LOGS_EMAIL = "";
+}
